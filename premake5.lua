a2f1380c50182ec12bf9749bd752e370f05c70d7
Peter Weinreich <pwein@web.de>
2025-12-27 19:27:21 +0100

    re-add premake setup after sync with master

---
+++ premake5.lua
@@ -0,0 +1,39 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++23"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imgui.h",
+		"imgui.cpp",
+		
+		"imconfig.h",	
+		"imgui_internal.h",
+		"imgui_rectpack.h",
+		"imgui_textedit.h",
+		"imgui_truetype.h",		
+		"imgui_draw.cpp",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",		
+		"imgui_demo.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+	
+	filter "system:linux"
+		pic "on"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
